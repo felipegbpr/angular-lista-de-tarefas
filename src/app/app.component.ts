204d9f0954be59c8f0043b34fc464b99bf0830c7
Felipe Borges <felipetecacc@gmail.com>
2024-09-12 21:22:28 -0300

    [HTML] Page List Component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,7 +5,7 @@ import { RouterOutlet } from '@angular/router';
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
-  template: `<router-outlet /> <p>Teste</p>`,
+  template: `<router-outlet />`,
 })
 export class AppComponent {
   title = 'curso-angular-lista-de-tarefas';
