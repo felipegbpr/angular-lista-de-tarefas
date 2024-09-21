3c9bf4559c2b7589ac6d05a3554f9ad4d8fcaed0
Felipe Borges <felipetecacc@gmail.com>
2024-09-21 16:35:06 -0300

    Sweet Alert 2

--- src/app/modules/to-do-list/pages/list/list.component.ts
+++ src/app/modules/to-do-list/pages/list/list.component.ts
@@ -1,4 +1,6 @@
 import { Component, signal } from '@angular/core';
+import Swal from 'sweetalert2';
+
 
 // Components
 import { InputAddItemComponent } from '../../components/input-add-item/input-add-item.component';
@@ -91,16 +93,38 @@ export class ListComponent {
   }
 
   public deleteItem(id: string) {
-    this.#setListItems.update((oldValue: IListItems[]) => {
-      console.log(id);
-      return oldValue.filter((res) => res.id !== id);
-    });
 
-    return this.#updateLocalStorage();
+    Swal.fire({
+      title: 'Tem certeza?',
+      text: 'Você não poderá reverter isso!',
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonText: 'Sim, delete o item'
+    }).then((result) => {
+      if (result.isConfirmed) {
+        this.#setListItems.update((oldValue: IListItems[]) => {
+          console.log(id);
+          return oldValue.filter((res) => res.id !== id);
+        });
+
+        return this.#updateLocalStorage();
+      }
+    });
   }
 
   public deleteAllItems() {
-    localStorage.removeItem(ELocalStorage.MY_LIST);
-    return this.#setListItems.set(this.#parseItems());
+
+    Swal.fire({
+      title: 'Tem certeza?',
+      text: 'Você não poderá reverter isso!',
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonText: 'Sim, delete tudo'
+    }).then((result) => {
+      if (result.isConfirmed) {
+        localStorage.removeItem(ELocalStorage.MY_LIST);
+        return this.#setListItems.set(this.#parseItems());
+      }
+    });
   }
 }
